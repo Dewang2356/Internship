297ee41199d860125882cda7c9d328e921910793
Dewang Seth <76672107+Dewang2356@users.noreply.github.com>
2025-02-15 16:49:01 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ var navbar = document.querySelector(".navbar");
 var open = document.querySelector(".mobileNavToggle");
 var navigate = document.querySelectorAll(".navigate");
 var scroll = document.querySelector(".scroll");
-
+var pulseBtn = document.querySelector(".pulseBtn");
+var vClose = document.querySelector(".vClose");
+var vLoader = document.querySelector(".vLoader");
+var ytVideo = document.querySelector(".ytVideo");
 var mobileMenu = document.querySelector('.mobileMenu');
 var cover = document.querySelector('.cover');
 var close = document.querySelector('.close');
 var tp = document.getElementsByClassName('tp');
+var myiFrame = document.querySelector("#myiFrame");
 const root = document.querySelector(":root");
 root.style.setProperty("--pseudo-top", navLine + 'px');
 
@@ -49,8 +53,27 @@ window.addEventListener('scroll', () => {
 open.addEventListener('click', () => {
     mobileMenu.style.display = "block";
     cover.style.display = "block";
-})
+});
 close.addEventListener('click', () => {
     mobileMenu.style.display = "none";
     cover.style.display = "none";
-})
+});
+pulseBtn.addEventListener('click', () => {
+    myiFrame.src = "https://www.youtube.com/embed/Y7f98aduVJ8?si=147gtROhAwaDzDOZ?autoplay=1";
+  vLoader.style.display = "block";
+   root.style.setProperty("--pseudo-close", 'block');
+   root.style.setProperty("--pseudo-close-flex", 'flex');
+   open.style.display = "none";
+
+});
+vClose.addEventListener('click', () => {
+    myiFrame.src = "";
+   open.style.display = "block";
+   root.style.setProperty("--pseudo-close", 'none');
+root.style.setProperty("--pseudo-close-flex", 'none');
+});
+
+
+myiFrame.addEventListener( "load", function(e) {
+vLoader.style.display = "none";
+} );
